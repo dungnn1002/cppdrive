780e160fa3a4267146445a1b4c2f0ffafbc813a9
betty2310 <huynh.dh2310@gmail.com>
2023-11-17 11:33:35 +0700

    feat(*): initial commit

--- include/example.h
+++ include/example.h
@@ -1,28 +1,21 @@
 #pragma once
 
+#include <string>
+
 /**
  * This is a dummy class to demonstrate features of the boiler plate.
  */
 class Dummy {
-	public:
-
-  /**
-   * Default constructor for Dummy (does nothing).
-   */
-  Dummy();
-  /**
-   * Returns a bool.
-   * @return Always True.
-   */
-  bool doSomething();
+   public:
+    /**
+     * Default constructor for Dummy (does nothing).
+     */
+    Dummy();
+    /**
+     * Here is how to comment a function.
+     * @param name you can use @ param to demonstrate what a function takes as a parameter.
+     * @return You can use @ return to demonstrate what a function returns. Here, we return nothing
+     *  because `void`.
+     */
+    void sayHelloTo(std::string name);
 };
-
-
-#ifdef ENABLE_DOCTEST_IN_LIBRARY
-#include "doctest/doctest.h"
-TEST_CASE("we can have tests in headers if we want")
-{
-    Dummy d;
-    CHECK(d.doSomething() == true);
-}
-#endif
